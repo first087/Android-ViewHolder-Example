8bade3151ce976d02b727a5f91783a623c45d975
Artit Kiuwilai <first087@gmail.com>
2015-07-08 09:59:46 +0700

    Prepare GridView Example

---
+++ app/src/main/java/com/artitk/android_viewholder_example/GridViewActivity.java
@@ -0,0 +1,13 @@
+package com.artitk.android_viewholder_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GridViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_view);
+    }
+}
